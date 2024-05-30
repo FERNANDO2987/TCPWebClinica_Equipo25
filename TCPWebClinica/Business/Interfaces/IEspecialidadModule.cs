bd938d010e8fa63a6b544bc0293f2ebf01ac7ce1
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-29 22:45:17 -0300

    Add: se agrega Interfaces para conexion a Base de Datos

---
+++ TCPWebClinica/Business/Interfaces/IEspecialidadModule.cs
@@ -0,0 +1,18 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+     public interface IEspecialidadModule
+    {
+        List<Especialidad> listarEspecialidad();
+        Especialidad agregarEspecialidad(Especialidad especialidad);
+        bool eliminarEspecilidad(int id);
+        void modificarEspecilidad(Especialidad especialidad);
+
+    }
+}
