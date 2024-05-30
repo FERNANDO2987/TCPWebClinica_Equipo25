1993e22c0a09cb484206f2a25effedf17ecd0330
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-29 22:21:31 -0300

    Add: Clase Turno

---
+++ TCPWebClinica/Business/Models/Turno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+     public class Turno
+    {
+
+        public int Id { get; set; }
+        public DateTime FechaHora { get; set; }
+        public Medico Medico { get; set; } //Relacion con el Medico
+        public Paciente Paciente { get; set; } //Relacion con el Paciente
+        public Especialidad Especialidad { get; set; } //Relacion con la Especilaidad
+        public string Observaciones { get; set; }
+        public EstadoTurno Estado { get; set; } //Relacion con el Estado del Turno
+        public ObraSocial ObraSocial { get; set; }  // Relación con ObraSocial
+
+    }
+}
