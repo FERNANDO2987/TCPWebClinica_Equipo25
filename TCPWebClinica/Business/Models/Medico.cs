912b5045888092e36af7d6733d44df8ba9565f9f
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-29 22:21:52 -0300

    Add: Clase Medico

---
+++ TCPWebClinica/Business/Models/Medico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+     public class Medico
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public List<Especialidad> Especialidades { get; set; }
+        public List<Turno> Turnos { get; set; }
+        public List<HorarioDeTrabajo> HorarioDeTrabajo { get; set; }
+
+    }
+}
