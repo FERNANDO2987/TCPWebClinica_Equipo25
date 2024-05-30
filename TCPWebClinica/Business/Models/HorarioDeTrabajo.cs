125c8da8648bb9ba746d1b0570cfd6b1a08d8db1
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-29 22:21:11 -0300

    Add: Clase Horario de Trabajo

---
+++ TCPWebClinica/Business/Models/HorarioDeTrabajo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class HorarioDeTrabajo
+    {
+        public int Id { get; set; }
+        public TimeSpan HoraEntrada { get; set; }
+        public TimeSpan HoraSalida { get; set; }
+
+    }
+}
