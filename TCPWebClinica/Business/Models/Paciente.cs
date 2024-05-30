7363ac79267b4c5097594fd17adb95f6be1ecb68
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-29 22:22:33 -0300

    Add: Clase Paciente

---
+++ TCPWebClinica/Business/Models/Paciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+     public class Paciente
+    {
+        public int IdPaciente { get; set; }
+        public int HistoriaClinica { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Documento { get; set; }
+        public DateTime FechaNacimeinto { get; set; }
+        public string Celular {  get; set; }
+        public string Email {  get; set; }
+        public bool Sexo { get; set; }
+        public ObraSocial ObraSocial { get; set; }  // Relación con ObraSocial
+        public List<Turno> Turnos { get; set; } //Relacion con Turno
+
+        
+    }
+}
