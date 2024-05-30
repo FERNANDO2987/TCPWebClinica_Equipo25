74138df9526b91ad7d65c60a9f3fbc98b5ec189f
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-29 22:45:42 -0300

    Add: se agrega Metodo listar Especilidad

---
+++ TCPWebClinica/Business/Modules/EspecialidadModule.cs
@@ -0,0 +1,68 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Modules
+{
+    public class EspecialidadModule : IEspecialidadModule
+    {
+        private readonly IAccesoDatos _accesoDatos;
+
+        public EspecialidadModule(IAccesoDatos accesoDatos)
+        {
+            _accesoDatos = accesoDatos;
+        }
+        public Especialidad agregarEspecialidad(Especialidad especialidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool eliminarEspecilidad(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Especialidad> listarEspecialidad()
+        {
+            var result = new List<Especialidad>();
+            try
+            {
+                _accesoDatos.setearConsulta("Select Id,Codigo,Nombre,Descripcion,IdMarca,IdCategoria,Precio  From ARTICULOS");
+                _accesoDatos.ejecutarLectura();
+
+                while (_accesoDatos.Lector.Read())
+                {
+                    Especialidad aux = new Especialidad();
+                    aux.Id = (int)_accesoDatos.Lector["Id"];
+                    aux.Nombre = (string)_accesoDatos.Lector["Nombre"];
+
+
+                    result.Add(aux);
+
+                }
+
+                return result;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error de conexion a SQL: " + ex.Message);
+            }
+            finally
+            {
+                _accesoDatos.cerrarConexion();
+            }
+        }
+
+        public void modificarEspecilidad(Especialidad especialidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
