bcbb02c2d5bcabb0b1293d3bf5aca39eb1eb265c
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-04 22:59:42 -0300

    Add:se agrega nuevo stored procedured

---
+++ TCPWebClinica/SQL/StoredProcedure/StoredProcedure.sql
@@ -0,0 +1,14 @@
+﻿USE TCPClinica_DB
+GO
+/****** Object:  StoredProcedure [dbo].[ObtenerEspecialidad]    Script Date: 04/06/2024 21:47:54 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ObtenerEspecialidad]
+AS
+BEGIN	
+	select * from [dbo].[Especialidad]  
+	where Deleted = 0
+  
+END
