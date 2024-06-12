846d73f09a2d8698e81dfce4845f38f7d5c19797
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-11 21:14:39 -0300

    Add: se agregan Interfaces para EstadoTurno y Turno

---
+++ TCPWebClinica/Business/Interfaces/IEstadoTurno.cs
@@ -0,0 +1,17 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IEstadoTurno
+    {
+        List<EstadoTurno> listarEstadosTurno();
+        EstadoTurno agregarEstadoTurno(EstadoTurno turno);
+        bool eliminarEstadoTurno(int id);
+
+    }
+}
