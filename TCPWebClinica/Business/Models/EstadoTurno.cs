c45b57b96464264f5341fbdb9d8ec689e13b86da
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-11 21:15:05 -0300

    Change: se modifica la clase EstadoTurno

--- TCPWebClinica/Business/Models/EstadoTurno.cs
+++ TCPWebClinica/Business/Models/EstadoTurno.cs
@@ -9,6 +9,7 @@ namespace Business.Models
     public class EstadoTurno
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
     }
 }
