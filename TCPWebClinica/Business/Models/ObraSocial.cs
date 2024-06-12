49395721ccdf5edb5107d2eaf0e7d72433da5a57
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-11 22:12:20 -0300

    Change: se modifica clase Obra Social

--- TCPWebClinica/Business/Models/ObraSocial.cs
+++ TCPWebClinica/Business/Models/ObraSocial.cs
@@ -11,5 +11,10 @@ namespace Business.Models
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public bool Activo { get; set; }
     }
 }
