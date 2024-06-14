2dd6196a13c463a31ca4c5cd01340ae209ee9847
Maximo Fernandez <maximo148601@gmail.com>
2024-06-13 23:37:16 -0300

    Eliminando (comentando) tablas duplicadas. Modificacion tabla estado turno

--- TCPWebClinica/SQL/Tablas/Tablas.sql
+++ TCPWebClinica/SQL/Tablas/Tablas.sql
@@ -58,6 +58,38 @@ CREATE TABLE [dbo].[Recepcionista](
 	[Deleted] [bit] NOT NULL,
 	[DeleteDate] [datetime] NULL
 	)
+--Medico
+
+Create Table[dbo].[Medico]
+(
+  [Id] [int] not null primary key identity(1,1),
+  [Nombre] nvarchar(100) NOT NULL,
+  [Apellido] nvarchar (100) NOT NULL,
+  [Email] nvarchar(100) NOT NULL,
+  [CreatedDate] [datetime] NOT NULL,
+  [Deleted] [bit] NOT NULL,
+  [DeleteDate] [datetime] NULL
+   
+)
+--Paciente
+
+--Create Table[dbo].[Paciente]
+--(
+--  [Id] [int] not null primary key identity(1,1),
+--  [HC] int NOT NULL,
+--  [Nombre] nvarchar (100) NOT NULL,
+--  [Apellido] nvarchar (100) NOT NULL,
+--  [Documento] int NOT NULL,
+--  [FechaNacimiento] datetime NOT NULL,
+--  [Celular] nvarchar (100) NOT NULL,
+--  [Email] nvarchar (100) NULL,
+--  [Sexo] bit NOT NULL,
+--  [IdObraSocial] int NOT NULL foreign key references ObraSocial(Id),
+--  [CreatedDate] [datetime] NOT NULL,
+--  [Deleted] [bit] NOT NULL,
+--  [DeleteDate] [datetime] NULL
+   
+--)
 CREATE TABLE [dbo].[Pacientes](
 	[Id] [int] NOT NULL PRIMARY KEY IDENTITY(1,1),
     [Apellido] [varchar](50) NOT NULL,
@@ -72,13 +104,13 @@ CREATE TABLE [dbo].[Pacientes](
 	[Deleted] [bit] NOT NULL,
 	[DeleteDate] [datetime] NULL
 )
-CREATE TABLE [dbo].[EstadoTurno](
-	[Id] [int] NOT NULL PRIMARY KEY IDENTITY(1,1),
-	[Estado] [varchar](50) NOT NULL,
-	[CreatedDate] [datetime] NOT NULL,
-	[Deleted] [bit] NOT NULL,
-	[DeleteDate] [datetime] NULL
-)
+--CREATE TABLE [dbo].[EstadoTurno](
+--	[Id] [int] NOT NULL PRIMARY KEY IDENTITY(1,1),
+--	[Estado] [varchar](50) NOT NULL,
+--	[CreatedDate] [datetime] NOT NULL,
+--	[Deleted] [bit] NOT NULL,
+--	[DeleteDate] [datetime] NULL
+--)
 create table [dbo].[Medicos](
 	[Id] [int] NOT NULL FOREIGN KEY REFERENCES Usuarios(Id) PRIMARY KEY,
 	[Nombre] [varchar](50) NOT NULL,
@@ -93,6 +125,17 @@ create table [dbo].[Especialidades_X_Medico](
 	[IdEspecialidad] [int] not null foreign key references Especialidad(id),
 	primary key (IdMedico, IdEspecialidad)
 )
+--Estado de Turno
+Create Table[dbo].[EstadoTurno]
+(
+  [Id] int Identity(1,1) Primary Key,
+  [Codigo] Nvarchar(50) NOT NULL,
+  [Descripcion] Nvarchar(100) NOT NULL,
+  [CreatedDate] [datetime] NOT NULL,
+  [Deleted] [bit] NOT NULL,
+  [DeleteDate] [datetime] NULL,
+   
+)
 create table [dbo].[Turnos](
 	[Id] [int] not null primary key identity(1,1),
 	[IdPaciente] [int] not null foreign key references Pacientes(id),
@@ -105,17 +148,7 @@ create table [dbo].[Turnos](
 )
 go
 
---Estado de Turno
-Create Table[dbo].[EstadoTurno]
-(
-  [Id] int Identity(1,1) Primary Key,
-  [Codigo] Nvarchar(50) NOT NULL,
-  [Nombre] Nvarchar(100) NOT NULL,
-  [CreatedDate] [datetime] NOT NULL,
-  [Deleted] [bit] NOT NULL,
-  [DeleteDate] [datetime] NULL,
-   
-)
+
 
 --HorarioDeTrabajo
 
@@ -131,40 +164,10 @@ Create Table[dbo].[HorarioDeTrabajo]
 )
 
 
---Medico
-
-Create Table[dbo].[Medico]
-(
-  [Id] [int] not null primary key identity(1,1),
-  [Nombre] nvarchar(100) NOT NULL,
-  [Apellido] nvarchar (100) NOT NULL,
-  [Email] nvarchar(100) NOT NULL,
-  [CreatedDate] [datetime] NOT NULL,
-  [Deleted] [bit] NOT NULL,
-  [DeleteDate] [datetime] NULL
-   
-)
 
 
---Paciente
 
-Create Table[dbo].[Paciente]
-(
-  [Id] [int] not null primary key identity(1,1),
-  [HC] int NOT NULL,
-  [Nombre] nvarchar (100) NOT NULL,
-  [Apellido] nvarchar (100) NOT NULL,
-  [Documento] int NOT NULL,
-  [FechaNacimiento] datetime NOT NULL,
-  [Celular] nvarchar (100) NOT NULL,
-  [Email] nvarchar (100) NULL,
-  [Sexo] bit NOT NULL,
-  [IdObraSocial] int NOT NULL foreign key references ObraSocial(Id),
-  [CreatedDate] [datetime] NOT NULL,
-  [Deleted] [bit] NOT NULL,
-  [DeleteDate] [datetime] NULL
-   
-)
+
 
 
 
