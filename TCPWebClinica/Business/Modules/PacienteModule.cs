ad013ff47d7a0f694ef2182f2ba7a4a309295106
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-13 22:27:23 -0300

    Add: se declaran metodos de Paciente usando la InterfazPaciente

---
+++ TCPWebClinica/Business/Modules/PacienteModule.cs
@@ -0,0 +1,33 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Modules
+{
+    public class PacienteModule : IPacienteModule
+    {
+        private readonly IAccesoDatos _accesoDatos;
+        public PacienteModule(IAccesoDatos accesoDatos)
+        {
+            _accesoDatos = accesoDatos;
+        }
+        public Paciente agregarPaciente(Paciente paciente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool eliminarPaciente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Paciente> listarPacientes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
