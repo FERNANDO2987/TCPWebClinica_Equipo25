4b261bd75edd0397ee424fab3a570a584d384449
Maximo Fernandez <maximo148601@gmail.com>
2024-06-13 23:59:46 -0300

    SP recepcionista agregar, modificar, eliminar y leer

---
+++ TCPWebClinica/SQL/StoredProcedure/SPRecepcionista.sql
@@ -0,0 +1,51 @@
+﻿USE TCPClinica_DB
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+--***********AGREGAR RECEPCIONISTA************--
+
+CREATE PROCEDURE [dbo].[AgregarRecepcionista]
+    @Id INT,
+    @Nombre VARCHAR(50),
+    @Apellido VARCHAR(50),
+    @Email VARCHAR(100)
+AS
+BEGIN
+    INSERT INTO [dbo].[Recepcionista] (Id, Nombre, Apellido, Email, CreatedDate, Deleted,DeleteDate)
+    VALUES (@Id, @Nombre, @Apellido, @Email, GETDATE(), 0 , null)
+END
+go
+--***********ELIMINAR RECEPCIONISTA************--
+CREATE PROCEDURE [dbo].[EliminarRecepcionista]
+    @Id INT
+AS
+BEGIN
+    UPDATE [dbo].[Recepcionista]
+    SET Deleted = 1, DeleteDate = GETDATE()
+    WHERE Id = @Id and Deleted = 0
+END
+go
+--************MODIFICAR RECEPCIONISTA***********--
+CREATE PROCEDURE [dbo].[ModificarRecepcionista]
+    @Id INT,
+    @Nombre VARCHAR(50),
+    @Apellido VARCHAR(50),
+    @Email VARCHAR(100)
+AS
+BEGIN
+    UPDATE [dbo].[Recepcionista]
+    SET Nombre = @Nombre, Apellido = @Apellido, Email = @Email
+    WHERE Id = @Id and Deleted = 0
+END
+go
+--***********LEER RECEPCIONISTA************--
+CREATE PROCEDURE [dbo].[LeerRecepcionista]
+AS
+BEGIN
+    SELECT Id, Nombre, Apellido, Email
+    FROM [dbo].[Recepcionista]
+	WHERE Deleted = 0
+END
