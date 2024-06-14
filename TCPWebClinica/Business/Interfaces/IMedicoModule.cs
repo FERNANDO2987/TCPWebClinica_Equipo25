40926a81b077b99d958fdf68d1e80f7a60202cf0
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-13 22:07:37 -0300

    Add: se agrega Interfaz de Medico

---
+++ TCPWebClinica/Business/Interfaces/IMedicoModule.cs
@@ -0,0 +1,17 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IMedicoModule
+    {
+        List<Medico> listarMedicos();
+        Medico agregarMedico(Medico medico);
+        bool eliminarMedico(int id);
+
+    }
+}
