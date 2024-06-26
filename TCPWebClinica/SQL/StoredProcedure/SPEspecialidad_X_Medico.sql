ebe2c802a3379f1560199895ac3024b4ad46e6ae
Fernando2987 <fernandopalacios51@gmail.com>
2024-06-25 22:54:54 -0300

    Add: se agrega nuevo Stored Procedure para obtener la especilidad por Medico

---
+++ TCPWebClinica/SQL/StoredProcedure/SPEspecialidad_X_Medico.sql
@@ -0,0 +1,22 @@
+﻿
+USE [TCPClinica_DB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ObtenerEspecialidadPorMedico](
+@IdMedico INT
+)
+AS
+BEGIN	
+	Set NOCOUNT ON;
+
+	Select E.Id, E.Nombre
+	From Especialidad E
+	INNER JOIN Especialidades_X_Medico EXM ON E.Id = EXM.IdEspecialidad
+	Where EXM.IdMedico = @IdMedico
+
+  
+END
