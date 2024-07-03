4ebf33122baf121572c494ae91504772c8c75db1
Fernando2987 <fernandopalacios51@gmail.com>
2024-07-02 23:08:09 -0300

    Add: se agrega metodo en javaScriipt para activar y desactiba nueva busqueda

---
+++ TCPWebClinica/WebApp/Script/activarBotonBuscar.js
@@ -0,0 +1,14 @@
+﻿function activarBotonBuscar() {
+    var btnBuscar = document.getElementById('<%= btnBuscarPaciente.ClientID %>');
+    btnBuscar.disabled = false;
+}
+
+// Llamada inicial para deshabilitar el botón si hay un paciente seleccionado
+window.onload = function () {
+    var selectedPatientName = document.getElementById('<%= txtNombreApellidoPaciente.ClientID %>').value.trim();
+    var btnBuscar = document.getElementById('<%= btnBuscarPaciente.ClientID %>');
+
+    if (selectedPatientName !== '') {
+        btnBuscar.disabled = true;
+    }
+};
