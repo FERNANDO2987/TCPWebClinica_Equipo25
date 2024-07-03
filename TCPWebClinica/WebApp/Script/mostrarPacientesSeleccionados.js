740ae95946ddde626e34afab29e45f0e1af03fd5
Fernando2987 <fernandopalacios51@gmail.com>
2024-07-02 23:09:04 -0300

    Add: se agrega nuevo metodo en javaScript para que muestre la tabal cuando uno requiere

---
+++ TCPWebClinica/WebApp/Script/mostrarPacientesSeleccionados.js
@@ -0,0 +1,4 @@
+﻿function mostrarPacienteSeleccionado() {
+    document.getElementById('<%= gvPacientes.ClientID %>').style.display = 'none';
+    document.getElementById('selectedPatientDetails').style.display = 'block';
+}
