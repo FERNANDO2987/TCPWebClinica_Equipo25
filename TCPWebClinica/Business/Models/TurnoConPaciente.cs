b2b61e849c5ba44efb8870f587dc258230660263
Fernando2987 <fernandopalacios51@gmail.com>
2024-07-04 21:59:39 -0300

    Add: se agrega nuevo metodo para obtener el turno con su paciente

---
+++ TCPWebClinica/Business/Models/TurnoConPaciente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class TurnoConPaciente
+    {
+        public int Id { get; set; }
+        public string NombrePaciente { get; set; }
+        public string ApellidoPaciente { get; set; }
+        public string Observaciones { get; set; }
+        public DateTime FechaTurno { get; set; }
+        public string Especialidad { get; set; }
+        public string Medico { get; set; }
+        public string Descripcion { get; set; }
+        public string ObraSocial { get; set; }
+    }
+
+
+}
