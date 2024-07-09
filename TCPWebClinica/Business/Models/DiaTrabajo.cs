e2eb2b0eb728e9cabe8a9f0edc5d91b8462e65b4
Fernando2987 <fernandopalacios51@gmail.com>
2024-07-08 22:37:06 -0300

    Add: se agrega nueva clase para el dia de Trabajo

---
+++ TCPWebClinica/Business/Models/DiaTrabajo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class DiaTrabajo
+    {
+        public int Id { get; set; }
+        public Medico Medico { get; set; }
+        public DayOfWeek Dia { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+
+       
+    }
+}
