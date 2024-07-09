f4858f830bc7a0f0b3a4d4b35f14d4cb46c1182c
Fernando2987 <fernandopalacios51@gmail.com>
2024-07-08 22:36:37 -0300

    Add: se agrega nueva interfaz para los horariso de Trabajo

---
+++ TCPWebClinica/Business/Interfaces/IDiaTrabajoModule.cs
@@ -0,0 +1,21 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+     public interface IDiaTrabajoModule
+    {
+        List<DiaTrabajo> HorarioTrabajo();
+        List<DiaTrabajo> ObtenerHorarioTrabajoMedico();
+
+        DiaTrabajo AgregarHorariosYDiasDeTrabajo(DiaTrabajo diaTrabajo);
+
+        List<DiaTrabajo> ObtenerHorarioTrabajoMedicoPorMedico(int medicoId);
+
+
+    }
+}
